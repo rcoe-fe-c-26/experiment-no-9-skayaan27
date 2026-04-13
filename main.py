417eef67872fa8cb6098d32cdf1feb0b48c4db30
skayaan27 <skayaan@eng.rizvi.edu.in>
2026-04-13 18:01:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
-# AIM: Design a Python program to compute 
-# the factorial of a given integer N.
-# Coder:
-# Date:
+#Aim : Design a Python program to compute the factorial of a given integer N.
+#Coder: Shaikh Ayaan
+#Class: C (COMPS-01)
+#Date: 02 /02/2026
 
-print("--- Factorial Finder ---\n")
+print("---Factorial Finder---\n")
 
+#Write your code here
+num =int(input('Emter a number:'))
+print("")
+og_num=num
+for a in range(1,num):
+    num=num*a
+print(f"Factorial of {og_num} is {num}")
 
-# Write your code here
